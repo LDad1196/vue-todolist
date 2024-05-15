2ddceddd95c319f6942d2b5b5145e55bd2c7a15e
Lorenzo D'Addazio <ldaddazio11@gmail.com>
2024-05-15 16:56:36 +0200

    m1 completato

--- js/main.js
+++ js/main.js
@@ -4,11 +4,26 @@ createApp({
 
     data() {
         return {
-            
+            ToDoList: [
+                {titolo:"alzarsi", done: false},
+                {titolo:"fare colazione", done: false},
+                {titolo:"prendere il caffè", done: false},
+                {titolo:"lavarsi i denti", done: false},
+                {titolo:"entrare in call", done: false},
+            ],
+            newActivity:"",
         }
     },
     methods: {
-       
+       checkIfDone(index){
+        if(this.ToDoList[index].done == true) {
+            return "color-green";
+        }
+
+        else if (this.ToDoList[index].done == false) {
+            return "color-red"
+        }
+       }
     },
     mounted() {
        
